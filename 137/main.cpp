6e96c5c8b3da2946ef80e4b6838b1b3f46a6f5e0
mdan2000 <mdan_2000@mail.ru>
2022-07-23 20:23:37 +0300

    Complete solve of 137 [worked]

--- 137/main.cpp
+++ 137/main.cpp
@@ -1,4 +1,4 @@
-#include "../helper/helper.h"
+//#include "../helper/helper.h"
 
 #include <iostream>
 #include <vector>
@@ -12,6 +12,7 @@ void Floid(vector<vector<long long>>& g) {
     for (int k = 0; k < g.size(); ++k) {
         for (int i = 0; i < g.size(); ++i) {
             for (int j = 0; j < g.size(); ++j) {
+                if (g[i][k] != inf && g[k][j] != inf)
                 g[i][j] = min(inf, max(-inf, min(g[i][j], g[i][k] + g[k][j])));
             }
         }
@@ -27,12 +28,9 @@ int main() {
     for (auto& row : g) {
         for (auto& el : row) {
             cin >> el;
-        }
-    }
-
-    for (int i = 0; i < n; ++i) {
-        if (g[i][i] > 0) {
-            g[i][i] = 0;
+            if (el == 0) {
+                el = inf;
+            }
         }
     }
 
@@ -42,7 +40,7 @@ int main() {
     
     for (int i = 0; i < g.size(); ++i) {
         for (int j = 0; j < g.size(); ++j) {
-            if (g[i][j] != inf) {
+            if (g[i][j] <= 100*100) {
                 if (g[i][j] == newg[i][j] && newg[i][j] != -inf) {
                     cout << 1;
                 } else {
@@ -58,10 +56,10 @@ int main() {
         cout << endl;
     }
 
-    cout << endl;
-    printMatrix(g);
-    cout << endl;
-    printMatrix(newg);
+//    cout << endl;
+//    printMatrix(g);
+//    cout << endl;
+//    printMatrix(newg);
 
     return 0;
 }
