a10681a216ab667afe997276265e747c83cbd5ea
mdan2000 <mdan_2000@mail.ru>
2022-07-23 19:56:27 +0300

    Update solution to cycle on v to v [not worked]

--- 137/main.cpp
+++ 137/main.cpp
@@ -1,20 +1,18 @@
+#include "../helper/helper.h"
+
 #include <iostream>
 #include <vector>
 #include <algorithm>
 
 using namespace std;
 
-void Floid(vector<vector<int>>& g) {
+const long long inf = 1e9;
+
+void Floid(vector<vector<long long>>& g) {
     for (int k = 0; k < g.size(); ++k) {
         for (int i = 0; i < g.size(); ++i) {
             for (int j = 0; j < g.size(); ++j) {
-                if (g[i][k] && g[k][i]) {
-                    if (g[i][j] == 0) {
-                        g[i][j] = g[i][k] + g[k][j];
-                    } else {
-                        g[i][j] = min(g[i][j], g[i][k] + g[k][j]);
-                    }
-                }
+                g[i][j] = min(inf, max(-inf, min(g[i][j], g[i][k] + g[k][j])));
             }
         }
     }
@@ -24,33 +22,46 @@ int main() {
     int n;
     cin >> n;
 
-    vector<vector<int>> g(n, vector<int>(n, 0));
+    vector<vector<long long>> g(n, vector<long long>(n, inf));
 
     for (auto& row : g) {
         for (auto& el : row) {
             cin >> el;
         }
     }
-    
+
+    for (int i = 0; i < n; ++i) {
+        if (g[i][i] > 0) {
+            g[i][i] = 0;
+        }
+    }
+
     Floid(g);
-    vector<vector<int>> newg = g;
+    vector<vector<long long>> newg = g;
     Floid(newg);
     
     for (int i = 0; i < g.size(); ++i) {
         for (int j = 0; j < g.size(); ++j) {
-            if (g[i][j] != 0) {
-                if (g[i][j] == newg[i][j]) {
+            if (g[i][j] != inf) {
+                if (g[i][j] == newg[i][j] && newg[i][j] != -inf) {
                     cout << 1;
                 } else {
                     cout << 2;
                 }
-            } else {
+            } else if (i != j) {
                 cout << 0;
+            } else {
+                cout << 1;
             }
             cout << ' ';
         }
         cout << endl;
     }
+
+    cout << endl;
+    printMatrix(g);
+    cout << endl;
+    printMatrix(newg);
 
     return 0;
 }
