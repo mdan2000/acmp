44921f5a81d041a11f9a199423d4a797af665a50
mdan2000 <mdan_2000@mail.ru>
2022-07-24 16:37:06 +0300

    Added test to 951 [tests]

---
+++ 951/stress_tests.py
@@ -0,0 +1,15 @@
+
+
+def get_tests():
+    tests = [
+        '4 5\n2 2 1 4 5',
+        '2 2\n4 1 1 1 2 2 1 2 2'
+    ]
+    return tests
+
+def get_ans():
+    ans = [
+        '4',
+        '0'
+    ]
+    return ans
